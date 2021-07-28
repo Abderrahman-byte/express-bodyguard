d96768a412fe059565d9cb7c708b0ae9b3d00039
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-07-28 16:31:31 +0100

    Create Content Security Policy Middleware

---
+++ middlewares/headers/ContentSecurityPolicy.js
@@ -0,0 +1,27 @@
+const DEFAULT_POLICIES = {
+    'default-src': ['\'self\''],
+    'base-uri': ['\'self\''],
+    'block-all-mixed-content': [],
+    'font-src': ['\'self\'', 'https:', 'data:'],
+    'frame-ancestors': ['\'self\''],
+    'img-src': ['\'self\'', 'data:'],
+    'object-src': ['\'none\''],
+    'script-src': ['\'self\''],
+    'script-src-attr': ['\'none\''],
+    'style-src': ['\'self\'', 'https:'],
+    // 'upgrade-insecure-requests': [],
+}
+
+const ContentSecurityPolicy = (policies = {}) => {
+    const usedPolicies = { ...DEFAULT_POLICIES, ...policies }
+    const normalizedDirectives = Object.entries(usedPolicies).map((policy) => `${policy[0]} ${policy[1].join(' ')}`)
+    const headerValue = normalizedDirectives.join('; ')
+    const headerName = 'Content-Security-Policy'
+
+    return (request, response, next) => {
+        response.setHeader(headerName, headerValue)
+        if (next instanceof Function) next()
+    }
+}
+
+module.exports = ContentSecurityPolicy
