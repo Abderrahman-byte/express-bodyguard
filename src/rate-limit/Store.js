8e55360a0b66c4950c4e1cdff7bde1eef4f8c8d7
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-07-28 19:57:57 +0100

    Create Store constructor

---
+++ src/rate-limit/Store.js
@@ -0,0 +1,15 @@
+class Store {
+    incr() {
+        throw new Error('Incr function is not implemented')
+    }
+
+    decrement() {
+        throw new Error('decrement function is not implemented')
+    }
+
+    resetKey() {
+        throw new Error('resetKey function is not implemented')
+    }
+}
+
+module.exports = Store
